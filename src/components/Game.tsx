2bfd30533ca25ffea2ef130f42500cd530c9846f
Jayson Hewe <jhewe.github@gmail.com>
2023-04-10 03:19:37 +0100

    feat: remove redundant useState for xIsNext

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import Board from './Board'
 
 export default function Game() {
-	const [xIsNext, setXIsNext] = useState(true)
 	const [history, setHistory] = useState([Array(9).fill(null)])
 	const [currentMove, setCurrentMove] = useState(0)
+	const xIsNext = currentMove % 2 === 0
 	const currentSquares = history[currentMove]
 
 	function handlePlay(nextSquares: string[]): void {
@@ -12,12 +12,10 @@ export default function Game() {
 
 		setHistory(nextHistory)
 		setCurrentMove(nextHistory.length - 1)
-		setXIsNext(!xIsNext)
 	}
 
 	function jumpTo(nextMove: number): void {
 		setCurrentMove(nextMove)
-		setXIsNext(nextMove % 2 === 0)
 	}
 
 	const moves = history.map((_squares, move) => {
