ed7023cebda856aa8d6248c97552ce9c7c13b0ff
Jayson Hewe <jhewe.github@gmail.com>
2023-04-10 12:51:00 +0100

    feat: add movesList toggle

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,6 +4,8 @@ import Board from './Board'
 export default function Game() {
 	const [history, setHistory] = useState([Array(9).fill(null)])
 	const [currentMove, setCurrentMove] = useState(0)
+	const [sortAsc, setSortAsc] = useState(true)
+
 	const xIsNext = currentMove % 2 === 0
 	const currentSquares = history[currentMove]
 
@@ -18,22 +20,35 @@ export default function Game() {
 		setCurrentMove(nextMove)
 	}
 
+	function toggleSort(): void {
+		setSortAsc(!sortAsc)
+	}
+
 	const moves = history.map((_squares, move) => {
 		const desc = move ? `Go to move #${move}` : 'Go to game start'
-		return (
-			<li key={move}>
-				{move === currentMove ? (
-					<small>
-						<i>{desc}</i>
-					</small>
-				) : (
-					<button onClick={() => jumpTo(move)} type='button'>
-						{desc}
-					</button>
-				)}
-			</li>
-		)
+		return {
+			move,
+			desc,
+		}
 	})
+
+	if (!sortAsc) {
+		moves.reverse()
+	}
+
+	const movesList = moves.map(({ move, desc }) => (
+		<li key={move}>
+			{move === currentMove ? (
+				<small>
+					<i>{desc}</i>
+				</small>
+			) : (
+				<button onClick={() => jumpTo(move)} type='button'>
+					{desc}
+				</button>
+			)}
+		</li>
+	))
 
 	return (
 		<div className='game'>
@@ -45,7 +60,10 @@ export default function Game() {
 				/>
 			</div>
 			<div className='game-info'>
-				<ol>{moves}</ol>
+				<button onClick={toggleSort} type='button'>
+					{sortAsc ? 'Sort Descending' : 'Sort Ascending'}
+				</button>
+				<ol>{movesList}</ol>
 			</div>
 		</div>
 	)
